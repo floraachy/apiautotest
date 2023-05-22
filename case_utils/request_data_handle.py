9c353adaa1baeddba1a3172a1b21aeb20f24297e
floraachy <1622042529@qq.com>
2023-05-22 09:34:27 +0800

    优化封装用例请求处理方法，增加支持allure报告显示

--- case_utils/request_data_handle.py
+++ case_utils/request_data_handle.py
@@ -7,11 +7,13 @@
 # @Desc: 处理request请求前后的用例数据
 
 
-from case_utils.data_handle import eval_data_process, case_data_replace
+from common_utils.data_handle import eval_data_process, data_replace
 from config.global_vars import GLOBAL_VARS
 from requests import Response
 from loguru import logger
-from case_utils.data_handle import json_extractor, re_extract
+from common_utils.data_handle import json_extractor, re_extract
+from case_utils.allure_handle import allure_step
+from common_utils.base_request import BaseRequest
 
 
 # ---------------------------------------- 请求前的数据处理----------------------------------------#
@@ -22,6 +24,19 @@ class RequestPreDataHandle:
     """
 
     def __init__(self, request_data):
+        logger.info(f"\n======================================================\n" \
+                    "-------------Start：处理用例数据前--------------------\n"
+                    f"用例标题: {request_data.get('title', None)}\n" \
+                    f"请求路径: {request_data.get('url', None)}\n" \
+                    f"请求方式: {request_data.get('method', None)}\n" \
+                    f"请求头:   {request_data.get('headers', None)}\n" \
+                    f"请求关键字: {request_data.get('pk', None)}\n" \
+                    f"请求内容: {request_data.get('payload', None)}\n" \
+                    f"请求文件: {request_data.get('files', None)}\n" \
+                    f"后置提取参数: {request_data.get('extract', None)}\n" \
+                    f"响应断言: {request_data.get('assert_response', None)}\n" \
+                    f"数据库断言: {request_data.get('assert_sql', None)}\n" \
+                    "=====================================================")
         self.request_data = request_data
 
     def request_data_handle(self):
@@ -35,6 +50,20 @@ class RequestPreDataHandle:
         self.files_handle()
         self.extract_handle()
         self.assert_handle()
+        logger.info(f"\n======================================================\n" \
+                    "-------------End：处理用例数据后--------------------\n"
+                    f"用例标题: {self.request_data.get('title', None)}\n" \
+                    f"请求路径: {self.request_data.get('url', None)}\n" \
+                    f"请求方式: {self.request_data.get('method', None)}\n" \
+                    f"请求头:   {self.request_data.get('headers', None)}\n" \
+                    f"请求关键字: {self.request_data.get('pk', None)}\n" \
+                    f"请求内容: {self.request_data.get('payload', None)}\n" \
+                    f"请求文件: {self.request_data.get('files', None)}\n" \
+                    f"后置提取参数: {self.request_data.get('extract', None)}\n" \
+                    f"响应断言: {self.request_data.get('assert_response', None)}\n" \
+                    f"数据库断言: {self.request_data.get('assert_sql', None)}\n" \
+                    "=====================================================")
+
         return self.request_data
 
     def url_handle(self):
@@ -43,8 +72,6 @@ class RequestPreDataHandle:
         """
         host = GLOBAL_VARS.get("host", "")
         url = self.request_data.get("url", "")
-        logger.info(
-            f"-----Start-----处理前的host： {GLOBAL_VARS.get('host', '')} || 处理前的url： {self.request_data.get('url', '')}")
         # 从用例数据中获取url，如果键url不存在，则返回空字符串
         # 如果url是以http开头的，则直接使用该url，不与host进行拼接
         if url.lower().startswith("http"):
@@ -63,78 +90,110 @@ class RequestPreDataHandle:
                 # 如果host不以/结尾 且 url不以/开头，则将host和url拼接起来的时候增加/，组成新的url
                 full_url = host + "/" + url
         self.request_data["url"] = full_url
-        logger.info(f"-----End-----处理完成后的full_url：{self.request_data['url']}")
+
+    def method_handle(self):
+        # TODO 暂时不需要处理，后续有需要在处理
+        pass
 
     def headers_handle(self):
         # 从用例数据中获取header， 处理header
-        logger.info(
-            f"-----Start-----处理前的headers： {type(self.request_data.get('headers', None))} ||  {self.request_data.get('headers', None)}")
         if self.request_data.get("headers", None):
-            self.request_data["headers"] = eval_data_process(case_data_replace(self.request_data.get("headers", None)))
-            logger.info(
-                f"-----End-----处理完成后的headers： {type(self.request_data['headers'])} ||  {self.request_data['headers']}")
-
-    def method_handle(self):
-        # 从用例数据中获取method， 处理method
-        logger.info(f"请求头：{self.request_data.get('method')}")
+            self.request_data["headers"] = eval_data_process(
+                data_replace(content=self.request_data.get("headers", None), source=GLOBAL_VARS))
 
     def payload_handle(self):
         # 处理请求参数payload
-        logger.info(
-            f"-----Start-----处理前的payload： {type(self.request_data.get('payload', None))} ||  {self.request_data.get('payload', None)}")
         if self.request_data.get("payload", None):
-            self.request_data["payload"] = eval_data_process(case_data_replace(self.request_data.get("payload", None)))
-            logger.info(
-                f"-----End-----处理完成后的payload： {type(self.request_data['payload'])} ||  {self.request_data['payload']}")
+            self.request_data["payload"] = eval_data_process(
+                data_replace(content=self.request_data.get("payload", None), source=GLOBAL_VARS))
 
     def files_handle(self):
         # 处理文件
-        logger.info(
-            f"-----Start-----处理前的files： {type(self.request_data.get('files', None))} ||  {self.request_data.get('files', None)}")
         # TODO 暂时还没想好怎么处理
+        pass
 
     def extract_handle(self):
-        # 处理后置提取参
-        logger.info(
-            f"-----Start-----处理前的extract： {type(self.request_data.get('extract', None))} ||  {self.request_data.get('extract', None)}")
+        # 处理后置提取参数
         if self.request_data.get("extract", None):
             # 仅提取参数中的python表达式，不需要进行数据替换
             self.request_data["extract"] = eval_data_process(self.request_data.get("extract", None))
-            logger.info(
-                f"-----End-----处理完成后的extract： {type(self.request_data['extract'])} ||  {self.request_data['extract']}")
 
     def assert_handle(self):
         # 处理响应断言参数
-        logger.info(
-            f"-----Start-----处理前的assert_response： {type(self.request_data.get('assert_response', None))} ||  {self.request_data.get('assert_response', None)}")
         if self.request_data.get("assert_response", None):
             self.request_data["assert_response"] = eval_data_process(
-                case_data_replace(self.request_data.get("assert_response", None)))
-            logger.info(
-                f"-----End-----处理完成后的assert_response： {type(self.request_data['assert_response'])} ||  {self.request_data['assert_response']}")
+                data_replace(content=self.request_data.get("assert_response", None), source=GLOBAL_VARS))
 
 
-# ---------------------------------------- 请求后的参数提取处理----------------------------------------#
-def after_extract(response: Response, extract) -> None:
+# ---------------------------------------- 进行请求，请求后的参数提取处理----------------------------------------#
+class RequestHandle:
     """
-    从响应数据中提取请求后的参数，并保存到全局变量中
-    :param response: request 响应对象
-    :param extract: 需要提取的参数字典 '{"k1": "$.data"}' 或 '{"k1": "data:(.*?)$"}'
-    :return:
+    进行请求，请求后的参数提取处理
     """
-    logger.info(f"-----Start-----请求后的参数提取处理，需要提取的参数：{extract}-----")
-    if extract:
-        if response_type(response) == "json":
-            # 如果响应数据是json格式，则将按照json方式对后置提取参数进行处理
-            res = response.json()
-            for k, v in extract.items():
-                GLOBAL_VARS[k] = json_extractor(res, v)
-        else:
-            # 如果响应数据是str格式，则将按照str方式对后置提取参数进行处理
-            res = response.text
-            for k, v in extract.items():
-                GLOBAL_VARS[k] = re_extract(res, v)
-    logger.info(f"-----End-----参数提取后，打印当前的全局变量:{GLOBAL_VARS}")
+
+    def __init__(self, case_data):
+        self.case_data = case_data
+
+    def send_request_extract(self):
+        """
+        发送请求并进行后置参数提取操作
+        """
+        response = BaseRequest.send_request(self.case_data)
+        self.case_data["extract"] = self.after_extract(response, self.case_data.get("extract", None))
+        # 从全局变量中获取最新值，替换数据库断言中的参数
+        if self.case_data.get('assert_sql', None):
+            self.case_data["assert_sql"] = eval_data_process(
+                data_replace(content=self.case_data["assert_sql"], source=GLOBAL_VARS))
+        logger.info(f"\n======================================================\n" \
+                    "-------------请求数据--------------------\n"
+                    f"用例标题: {self.case_data.get('title', None)}\n" \
+                    f"请求路径: {self.case_data.get('url', None)}\n" \
+                    f"请求方式: {self.case_data.get('method', None)}\n" \
+                    f"请求头:   {self.case_data.get('headers', None)}\n" \
+                    f"请求关键字: {self.case_data.get('pk', None)}\n" \
+                    f"请求内容: {self.case_data.get('payload', None)}\n" \
+                    f"请求文件: {self.case_data.get('files', None)}\n" \
+                    f"请求响应数据: {response.text}\n" \
+                    f"后置提取参数（新）: {self.case_data.get('extract', None)}\n" \
+                    f"数据库断言: {self.case_data.get('assert_sql', None)}\n" \
+                    "=====================================================")
+        allure_step(step_title=f"请求地址:{self.case_data['url']}")
+        allure_step(step_title=f"请求方式：{self.case_data['method']}")
+        allure_step(step_title="请求头", content=self.case_data['headers'])
+        allure_step(step_title="请求参数", content=self.case_data['payload'])
+        allure_step(step_title="请求文件", content=self.case_data['files'])
+        allure_step(step_title="后置提取参数（新）", content=self.case_data['extract'])
+        allure_step(step_title="请求响应数据", content=self.case_data['assert_response'])
+        allure_step(step_title="响应断言参数", content=self.case_data['assert_response'])
+        allure_step(step_title="数据库断言", content=self.case_data['assert_response'])
+        return response
+
+    def after_extract(self, response: Response, extract):
+        """
+        从响应数据中提取请求后的参数，并保存到全局变量中
+        :param response: request 响应对象
+        :param extract: 需要提取的参数字典 '{"k1": "$.data"}' 或 '{"k1": "data:(.*?)$"}'
+        :return:
+        """
+        logger.debug(f"-----Start-----请求后的参数提取处理，需要提取的参数：{extract}-----")
+        result = {}
+        if extract:
+            if response_type(response) == "json":
+                # 如果响应数据是json格式，则将按照json方式对后置提取参数进行处理
+                res = response.json()
+                for k, v in extract.items():
+                    result[k] = json_extractor(res, v)
+            else:
+                # 如果响应数据是str格式，则将按照str方式对后置提取参数进行处理
+                res = response.text
+                for k, v in extract.items():
+                    result[k] = re_extract(res, v)
+        logger.debug(f"-----End-----参数提取后，打印提取到的变量:{result}")
+        # 将提取到的变量保存在全局变量中
+        if result:
+            for k, v in result.items():
+                GLOBAL_VARS[k] = v
+        return result
 
 
 def response_type(response: Response) -> str:
